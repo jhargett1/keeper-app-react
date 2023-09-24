1afe76325d176a30c0e80b83ba38c73fa1c0a9f9
jhargett1 <119984652+jhargett1@users.noreply.github.com>
2023-09-24 08:17:43 -0500

    Adding styling to keeper app

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Note(props) {
   function handleClick() {
@@ -11,7 +12,9 @@ function Note(props) {
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       {/* - Callback from the Note component to trigger a delete function. */}
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
